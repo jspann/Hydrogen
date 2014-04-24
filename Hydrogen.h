7c38f9b54fe9a5ca1d999d8743f8935c9adbc659
James S <thedeveloper4@gmail.com>
2014-04-23 22:58:29 -0400

    Moved the Version data to a new file

---
+++ Hydrogen.h
@@ -0,0 +1,15 @@
+//
+//  Hydrogen.h
+//  
+//
+//  Created by James Spann on 4/23/14.
+//
+//
+
+#ifndef _Hydrogen_h
+#define _Hydrogen_h
+
+#define MAJOR_VERSION 1
+#define MINOR_VERSION 01
+
+#endif
