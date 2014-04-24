6e754c0f2a19f7bc4e1ae578edcbbc05c6ad5cd0
James S <thedeveloper4@gmail.com>
2014-04-23 22:59:43 -0400

    Moved the parser to its own file

---
+++ Parser.h
@@ -0,0 +1,19 @@
+//
+//  Parser.h
+//  
+//
+//  Created by James Spann on 4/23/14.
+//
+//
+
+#ifndef ____Parser__
+#define ____Parser__
+
+#include <iostream>
+#include "Table.h"
+
+
+void parse(char* fname);
+
+
+#endif /* defined(____Parser__) */
