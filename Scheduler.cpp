c6e3e0c925b6969753e979569a9c0b43b5b47f68
James S <thedeveloper4@gmail.com>
2014-04-23 23:01:33 -0400

    Added a scheduler

---
+++ Scheduler.cpp
@@ -0,0 +1,9 @@
+//
+//  Scheduler.cpp
+//  
+//
+//  Created by James Spann on 4/23/14.
+//
+//
+
+#include "Scheduler.h"
---
+++ Scheduler.h
@@ -0,0 +1,14 @@
+//
+//  Scheduler.h
+//  
+//
+//  Created by James Spann on 4/23/14.
+//
+//
+
+#ifndef ____Scheduler__
+#define ____Scheduler__
+
+#include <iostream>
+
+#endif /* defined(____Scheduler__) */
