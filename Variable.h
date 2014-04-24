7fc2905832f032506c7bee78d5ec3a035fc71008
James S <thedeveloper4@gmail.com>
2014-04-23 23:00:32 -0400

    Added a new "Variable" data structure
    This is used to add new parameters and manipulate data with more ease

---
+++ Variable.h
@@ -0,0 +1,18 @@
+//
+//  Variable.h
+//  
+//
+//  Created by James Spann on 4/23/14.
+//
+//
+
+#ifndef _Variable_h
+#define _Variable_h
+
+struct Variable {
+    int value;
+    int datatype;//1 if a pointer ($3) 0 if integer (5)
+};
+
+
+#endif
