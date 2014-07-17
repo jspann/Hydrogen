64c38eebcb34a3478b04673c33f3f0acb4d6609f
James S <thedeveloper4@gmail.com>
2014-07-17 00:56:33 -0400

    Added new file for executing based on operands and opcode

---
+++ Execute.cpp
@@ -0,0 +1,45 @@
+//
+//  Execute.c
+//  
+//
+//  Created by James Spann on 6/30/14.
+//
+//
+#include <string.h>
+#include <stdio.h>
+#include "Front.h"
+#include "Parser.h"
+#include "Hydrogen.h"
+
+using namespace std;
+
+void runCode(string opcode){
+    /*if (operand.compare("defi") == 0) {
+        rect.addToTable('i',currentInt);
+    }else if (operand.compare("add") == 0) {
+        int jt = 0;
+        
+        if (paramPlace != 1 && paramPlace != 0) {
+            
+            for (int d = 1; d < paramPlace; d++) {
+                if (parameters[d].datatype == 1){
+                    jt +=rect.returnValues(parameters[d].value);
+                }else{
+                    jt +=parameters[d].value;
+                }
+                
+            }
+            
+        }
+        
+        
+        
+        rect.setValue(parameters[0],jt);
+    }else if (operand.compare("sub") == 0) {
+    }else if (operand.compare("call") == 0) {
+    }else{
+        cout << "Unknown operand: " << operand << endl;
+        cout << "Halting" << endl;
+        exit(1);
+    }*/
+}
