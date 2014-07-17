d6dc9b44b74934aa7fc5bc204f1725dd02a2b860
James S <thedeveloper4@gmail.com>
2014-07-17 00:55:45 -0400

    Changed where a new line was printed

--- vm.cpp
+++ vm.cpp
@@ -18,7 +18,7 @@ void badhalt(std::string reason){
     //Stop all of the threads
     //Print out error message
 #ifdef __APPLE__ && __MACH__
-    cout << "\033[1;31mError: \033[0m\n" << reason;
+    cout << "\033[1;31mError: \033[0m\n" << reason << endl;
 #else
     std::cout << "Error: " << reason << endl;
 #endif
