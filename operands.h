d41a3ff9374f84070aeee9298b7d3e43b0ce3904
James S <thedeveloper4@gmail.com>
2014-07-17 00:54:20 -0400

    Added file for operands (Still in progress)

---
+++ operands.h
@@ -0,0 +1,34 @@
+//
+//  operands.h
+//  
+//
+//  Created by James Spann on 7/10/14.
+//
+//
+
+#ifndef _operands_h
+#define _operands_h
+
+#define defi 2 //
+#define const 2 //
+
+#define add 3 // <assigned register> <first value> <second value>
+#define sub 3 // <assigned register> <first value> <second value>
+#define mult 2// <assigned register> <first value> <second value>
+#define div 2// <assigned register> <first value> <second value>
+#define mod 3// <assigned register> <first value> <second value>
+
+#define inc 1 //<register being incremented>
+#define dec 1 //<register being decrimented>
+
+#define coeq 2 //<first register> <second register/ second value>
+#define cogt 2 //<first register> <second register/ second value>
+#define colt 2 //<first register> <second register/ second value>
+
+#define goto 1 //<line number>
+#define ret 0 //return from function
+
+#define hlt 0 //Stops program
+
+
+#endif
