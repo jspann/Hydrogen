d5f549eb82c4680e138ee819013c8edd79dec877
James Spann <thedeveloper4@gmail.com>
2014-09-12 21:16:39 -0400

    Working on a mini stack implementation

---
+++ src/HStack.c
@@ -0,0 +1,31 @@
+//
+//  HThread.cpp
+//  
+//
+//  Created by James Spann on 4/23/14.
+//
+//
+
+#include <pthread.h>
+
+#include "HThread.h"
+//http://www.tutorialspoint.com/cplusplus/cpp_multithreading.htm
+void start(){
+    //pthread_create();
+}
+
+void pause(){
+    
+}
+
+void resume(){
+    
+}
+
+void stop(){
+    //pthread_exit (status);
+}
+
+void setPriority(int newPrior){
+    
+}
