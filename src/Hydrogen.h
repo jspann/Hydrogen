6b045a0c3df65a47b577d2d93281d2bd7aa548a4
James Spann <thedeveloper4@gmail.com>
2014-09-21 00:39:20 -0400

    Added a file which contains version info

---
+++ src/Hydrogen.h
@@ -0,0 +1,26 @@
+//
+//  Hydrogen.h
+//  
+//
+//  Created by James Spann on 4/23/14.
+//
+//
+
+#ifndef _Hydrogen_h
+#define _Hydrogen_h
+
+//Versions
+#define MAJOR_VERSION 1
+#define MINOR_VERSION 01
+#define BUILD_VERSION 1 //Incremented each time there is a new release
+
+
+#define FILE_VERSION 1
+
+//Data Types
+typedef	uint   BOOL;
+typedef	uint   byte;
+typedef	signed int  num;
+
+
+#endif
