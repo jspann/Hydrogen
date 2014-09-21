58076ae089f97e7150a26e7fa56197b51e603493
James Spann <thedeveloper4@gmail.com>
2014-09-21 00:38:06 -0400

    Updated the frontend

--- src/Front.cpp
+++ src/Front.cpp
@@ -10,24 +10,34 @@
 #include "Front.h"
 #include "Parser.h"
 #include "Hydrogen.h"
-
+#include "vm.h"
+#include "Flags.h"
 char *filename;
 void printversion(){
     printf("Hydrogen Version %i.%02i\n",MAJOR_VERSION,MINOR_VERSION);
+    printf("Hydrogen File Version: %i\n",FILE_VERSION);
     //printf("Hydrogen Version \033[1;31m%i.%02i\033[0m\n",MAJOR_VERSION,MINOR_VERSION);
     //std::cout << "\033[1;31mbold red text\033[0m\n";
+}
+
+void printFileVersion(){
+    printf("Hydrogen File Version: %i\n",FILE_VERSION);
+
 }
 
 void help(){
     printf("\nAvailable commands\n");
     printf("=================\n");
     printf("-v for version\n");
-    printf("-R for running a file\n");
+    printf("-R <filename> for running a file\n");
+//    printf("-l for logging output to log.txt\n");
+//    printf("-l <filename> for logging output to <filename> \n");
     printf("-? for this screen\n\n");
 }
 
 
 int main (int argc, char *argv[]) {
+    
     if (argv[1] == NULL){
         help();
     } else{
@@ -36,6 +46,12 @@ int main (int argc, char *argv[]) {
             printversion();
         }else if(strcmp(argv[1],"-R")==0){
             filename=argv[2];
+            
+            if (strcmp(argv[3],"-l")==0) {
+                isLog = true;
+            }else{
+                isLog = false;
+            }
             
             FILE *fp = fopen(filename,"r");
             if( fp ) {
