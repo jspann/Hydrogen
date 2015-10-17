5aff5e425708281002d1dbd7d4154df9edbcf5db
James Spann <thedeveloper4@gmail.com>
2015-10-16 22:37:38 -0400

    Redoing the parser

--- src/vm.cpp
+++ src/vm.cpp
@@ -1,27 +1,41 @@
 #include <iostream>
 #include <fstream>
+//#import "bool.h"
 #include "vm.h"
-#include "Table.h"
+#include "stack.h"
+#include "heap.h"
 
 #define KBLU  "\x1B[34m"
 
 
-int running = 1;//1 if the vm is running and 0 if it is halted
-using namespace std;
+bool running = 1;//1 if the vm is running and 0 if it is halted
+//using namespace std;
 
-void initVM(){
-    //Create initial thread
-    //Get how much memory is available for use and platform info (Clock Speed, io ports, etc.)
+
+void loadFile(char* filename){
+
 }
 
-void badhalt(std::string reason){
-    //Stop all of the threads
-    //Print out error message
+void initVM(){
+	//Create initial thread
+	Stack functionStack;
+	functionStack.initStack();
+
+	//Check magic numbers
+
+	while(1){
+		//get instruction
+	}
+}
+
+void badhalt(char* reason){
+	//Stop all of the threads
+	//Print out error message
 #ifdef __APPLE__ && __MACH__
-    cout << "\033[1;31mError: \033[0m\n" << reason << endl;
+	std::cout << "\033[1;31mError: \033[0m\n" << reason << std::endl;
 #else
-    std::cout << "Error: " << reason << endl;
+	std::cout << "Error: " << reason << std::endl;
 #endif
-    
-    exit(0);
+	
+	exit(0);
 }
