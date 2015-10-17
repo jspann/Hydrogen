657264192f1ea8ecca59d60d6c7d929045fdf402
James Spann <thedeveloper4@gmail.com>
2015-10-16 22:37:08 -0400

    Removing unnecessary files
    These files were not actively being used and are unnecessary to future
    builds.

--- src/Debug.c
+++
@@ -1,9 +0,0 @@
-//
-//  Debug.c
-//  
-//
-//  Created by James Spann on 6/23/14.
-//
-//
-
-#include <stdio.h>
--- src/Scheduler.cpp
+++
@@ -1,9 +0,0 @@
-//
-//  Scheduler.cpp
-//  
-//
-//  Created by James Spann on 4/23/14.
-//
-//
-
-#include "Scheduler.h"
